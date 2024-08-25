56daa45d1fa87b48e9084eade3aff508954b06b4
Max50204 <schneidermaxim45@yahoo.com>
2024-08-25 11:53:01 +0200

    Test

---
+++ .idea/src/Program.java
@@ -0,0 +1,13 @@
+public class Program {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount("John Doe", 1000.0);
+        System.out.println("Owner: " + account.getOwner());
+        System.out.println("Balance: " + account.getBalance());
+
+        account.deposit(500.0);
+        account.withdraw(200.0);
+        account.addInterest(5.0);
+
+        System.out.println("Текущий баланс: " + account.getBalance());
+    }
+}
